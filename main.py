200e1fdf56eb60306a32370bb0843cc146cc81e2
danielsyahputra13 <dnlshp@gmail.com>
2022-05-28 12:51:20 +0700

    fix service

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from preprocess import SpacyPreprocessor
-# from tensorflow.keras.preprocessing.text import tokenizer_from_json
-# from tensorflow.keras.preprocessing.sequence import pad_sequences
-from sklearn.preprocessing import LabelEncoder
-
-
 import tensorflow as tf
 import json
 import numpy as np
+import os
+
+from preprocess import SpacyPreprocessor
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
 
 
-# spacy_model = SpacyPreprocessor.load_model()
-# preprocessor = SpacyPreprocessor(spacy_model=spacy_model, lemmatize=True, remove_numbers=True, remove_stopwords=True)
-
+spacy_model = SpacyPreprocessor.load_model()
+preprocessor = SpacyPreprocessor(spacy_model=spacy_model, lemmatize=True, remove_numbers=True, remove_stopwords=True)
 
 app = FastAPI()
 
@@ -22,37 +21,37 @@ app = FastAPI()
 class Teks(BaseModel):
     desc: str
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-# @app.post("/predict/")
-# async def predict(teks: Teks):
-#     desc_cleaned = preprocessor.preprocess_text(teks.desc)
+@app.post("/predict/")
+async def predict(teks: Teks):
+    desc_cleaned = preprocessor.preprocess_text(teks.desc)
+    print(os.getcwd())
 
-#     with open('assets/tokenizer_with_counts_100.json') as f:
-#         data = json.load(f)
-#         tokenizer = tokenizer_from_json(data)
+    with open('assets/tokenizer_with_counts_100.json') as f:
+        data = json.load(f)
+        tokenizer = tokenizer_from_json(data)
    
-#     max_length = 100
-#     trunc_type = 'post'
-#     padding_type = 'post'
+    max_length = 100
+    trunc_type = 'post'
+    padding_type = 'post'
 
-#     pred_sequences = tokenizer.texts_to_sequences([desc_cleaned])
-#     pred_padded = pad_sequences(pred_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+    pred_sequences = tokenizer.texts_to_sequences([desc_cleaned])
+    pred_padded = pad_sequences(pred_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
 
-#     model = tf.keras.models.load_model('assets/GRU_dense_with_count_100.h5')
+    model = tf.keras.models.load_model('assets/GRU_dense_with_count_100.h5')
 
-#     result = model.predict(pred_padded)[0]
-#     idx = np.argmax(result)
+    result = model.predict(pred_padded)[0]
+    idx = np.argmax(result)
 
-#     encoder = LabelEncoder()
-#     encoder.classes_ = np.load('assets/encoder_with_count_100.npy', allow_pickle=True)
+    encoder = LabelEncoder()
+    encoder.classes_ = np.load('assets/encoder_with_count_100.npy', allow_pickle=True)
 
-#     condition = encoder.inverse_transform([idx])[0]
+    condition = encoder.inverse_transform([idx])[0]
 
-#     return {
-#             "desc": desc_cleaned,
-#             "disease": condition,
-#         }
+    return {
+            "desc": desc_cleaned,
+            "disease": condition,
+        }
