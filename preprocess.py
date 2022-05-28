b1b2998adf722499ef7e3e601b7e5794be72e0ee
danielsyahputra13 <dnlshp@gmail.com>
2022-05-28 23:11:49 +0700

    add yaml file

--- preprocess.py
+++ preprocess.py
@@ -2,15 +2,15 @@ import re
 import nltk
 nltk.download("punkt")
 nltk.download("stopwords")
-# nltk.download("wordnet")
-# nltk.download("omw-1.4")
+nltk.download("wordnet")
+nltk.download("omw-1.4")
 
-# from nltk.stem import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 
 stopwords = stopwords.words('english')
 
-# lemmatizer = WordNetLemmatizer()
+lemmatizer = WordNetLemmatizer()
 
 def clean_teks(teks):
     teks_clean = re.sub(r"\d+", " ", teks) 
@@ -24,12 +24,12 @@ def remove_stopwords(teks):
     cleaned = [word for word in nltk.word_tokenize(teks) if word not in stopwords]
     return " ".join(cleaned)
 
-# def lemmatization(teks):
-#     lemma = [lemmatizer.lemmatize(word) for word in nltk.word_tokenize(teks)]
-#     return " ".join(lemma)
+def lemmatization(teks):
+    lemma = [lemmatizer.lemmatize(word) for word in nltk.word_tokenize(teks)]
+    return " ".join(lemma)
 
 def pre_process(teks):
     teks_clean = clean_teks(teks)
     teks_clean = remove_stopwords(teks_clean)
-    # teks_clean = lemmatization(teks_clean)
+    teks_clean = lemmatization(teks_clean)
     return teks_clean
