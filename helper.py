a464a3ff4dc20a921bff54a1063a15c0a0bc8835
danielsyahputra13 <dnlshp@gmail.com>
2022-06-05 10:14:33 +0700

    refactor code

---
+++ helper.py
@@ -0,0 +1,57 @@
+import json
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
+
+def get_padded_text(tokenizer, desc_cleaned):
+    max_length = 100
+    trunc_type = 'post'
+    padding_type = 'post'
+
+    pred_sequences = tokenizer.texts_to_sequences([desc_cleaned])
+    pred_padded = pad_sequences(pred_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+    return pred_padded
+
+def load_stuff():
+
+    # Load tokenizer
+    with open('assets/tokenizer.json') as f:
+        data = json.load(f)
+        tokenizer = tokenizer_from_json(data)
+    
+    # Load model
+    model = tf.keras.models.load_model('assets/model.h5')
+
+    # Load encoder
+    encoder = LabelEncoder()
+    encoder.classes_ = np.load('assets/encoder.npy', allow_pickle=True)
+
+    # Load description
+    description = pd.read_pickle("data/description.pkl")
+
+    return tokenizer, model, encoder, description
+
+def get_prediction(model, pred_padded, encoder, description):
+    result = model.predict(pred_padded)[0]
+    idx = result.argsort()[-3:][::-1]
+
+    conditions = encoder.inverse_transform(idx)
+
+    result_json = []
+    for i in range(len(conditions)):
+        condition = conditions[i]
+        probability = float(result[idx[i]])
+        deskripsi = description[description['Condition'] == condition]['Deskripsi'].values[0]
+        result_json.append(
+            {
+                "disease": condition,
+                "probability": probability,
+                "deskripsi": deskripsi,
+            }
+        )
+    return result_json
--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
-import tensorflow as tf
-import json
-import numpy as np
-import pandas as pd
 import translators as ts
 
 from preprocess import SpacyPreprocessor
-from tensorflow.keras.preprocessing.text import tokenizer_from_json
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from sklearn.preprocessing import LabelEncoder
+from helper import *
 
 
 spacy_model = SpacyPreprocessor.load_model()
 preprocessor = SpacyPreprocessor(spacy_model=spacy_model, lemmatize=True, remove_numbers=True, remove_stopwords=True)
 
-app = FastAPI()
+tokenizer, model, encoder, description = load_stuff()
 
+app = FastAPI()
 
 class Teks(BaseModel):
     desc: str
@@ -28,53 +22,20 @@ async def root():
 
 @app.post("/predict/")
 async def predict(teks: Teks):
-    # Translate ke inggris
+
+    # Translate teks into english first
     translated_text = ts.google(teks.desc, to_language='en')
 
     # Preprocess
     desc_cleaned = preprocessor.preprocess_text(translated_text)
 
+    # Get padded text
+    pred_padded = get_padded_text(tokenizer, desc_cleaned)
+
     # Predict
-    with open('assets/tokenizer.json') as f:
-        data = json.load(f)
-        tokenizer = tokenizer_from_json(data)
-   
-    max_length = 100
-    trunc_type = 'post'
-    padding_type = 'post'
-
-    pred_sequences = tokenizer.texts_to_sequences([desc_cleaned])
-    pred_padded = pad_sequences(pred_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
-
-    model = tf.keras.models.load_model('assets/model.h5')
-
-    result = model.predict(pred_padded)[0]
-    idx = result.argsort()[-3:][::-1]
-
-    encoder = LabelEncoder()
-    encoder.classes_ = np.load('assets/encoder.npy', allow_pickle=True)
-
-    conditions = encoder.inverse_transform(idx)
-
-    ############################################
-
-    # load description and medicine
-    description = pd.read_pickle("data/description.pkl")
-
-    result_json = []
-    for i in range(len(conditions)):
-        condition = conditions[i]
-        probability = float(result[idx[i]])
-        deskripsi = description[description['Condition'] == condition]['Deskripsi'].values[0]
-        result_json.append(
-            {
-                "disease": condition,
-                "probability": probability,
-                "deskripsi": deskripsi,
-            }
-        )
+    result = get_prediction(model, pred_padded, encoder, description)
 
     return {
         "description": desc_cleaned,
-        "result": result_json
+        "result": result
     }
